0cf3f580dd4990c4d03c3b778b7878587e3b6763
baobaojianqiang <16230091@qq.com>
2018-08-19 10:52:28 +0800

    support API 28: hook H

--- HostApp/src/main/java/jianqiang/com/activityhook1/ams_hook/MockClass2.java
+++ HostApp/src/main/java/jianqiang/com/activityhook1/ams_hook/MockClass2.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 
+import java.util.List;
+
 import jianqiang.com.activityhook1.RefInvoke;
 
 /**
@@ -24,8 +26,11 @@ import jianqiang.com.activityhook1.RefInvoke;
         switch (msg.what) {
             // ActivityThread里面 "LAUNCH_ACTIVITY" 这个字段的值是100
             // 本来使用反射的方式获取最好, 这里为了简便直接使用硬编码
-            case 100:
+            case 100:   //for API 28以下
                 handleLaunchActivity(msg);
+                break;
+            case 159:   //for API 28
+                handleActivity(msg);
                 break;
         }
 
@@ -43,5 +48,21 @@ import jianqiang.com.activityhook1.RefInvoke;
 
         Intent target = raw.getParcelableExtra(AMSHookHelper.EXTRA_TARGET_INTENT);
         raw.setComponent(target.getComponent());
+    }
+
+    private void handleActivity(Message msg) {
+        // 这里简单起见,直接取出TargetActivity;
+        Object obj = msg.obj;
+
+        List<Object> mActivityCallbacks = (List<Object>) RefInvoke.getFieldObject(obj, "mActivityCallbacks");
+        if(mActivityCallbacks.size() > 0) {
+            String className = "android.app.servertransaction.LaunchActivityItem";
+            if(mActivityCallbacks.get(0).getClass().getCanonicalName().equals(className)) {
+                Object object = mActivityCallbacks.get(0);
+                Intent intent = (Intent)RefInvoke.getFieldObject(object, "mIntent");
+                Intent target = intent.getParcelableExtra(AMSHookHelper.EXTRA_TARGET_INTENT);
+                intent.setComponent(target.getComponent());
+            }
+        }
     }
 }
